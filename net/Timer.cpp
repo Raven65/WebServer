faf5dfd14e3d905b853f134a3b8c9a6b46831162
Raven65 <286506286@qq.com>
2019-12-12 09:38:13 +0800

    fix timer

--- net/Timer.cpp
+++ net/Timer.cpp
@@ -65,11 +65,16 @@ void TimerHeap::handleTimeout() {
     if (size != sizeof(uint64_t)) {
         LOG << "Timeout Error";
     }
+    
+    TimerPtr tnow = timerHeap_.top();
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    long now = ts.tv_sec * 1000 + ts.tv_nsec / 1000 / 1000;
     while (!timerHeap_.empty()) {
-        TimerPtr tnow = timerHeap_.top();
         if (tnow->isDeleted()) {
             timerHeap_.pop();
-        } else if (tnow->isTimeout()) {
+//            LOG <<"delete";
+        } else if (now > tnow->getTime()) {
             timerHeap_.pop();
             LOG << "Connection Timeout!";
             loop_->runInLoop(std::bind(&Timer::runCallBack, tnow));
